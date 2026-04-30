03de0d7dbb60d5cc5053514a0ba0ff564b605e64
Vignesh <vignesh@MacBook-Pro.local>
2026-05-01 01:46:50 +0530

    Restructuring

---
+++ core/logger.py
@@ -0,0 +1,29 @@
+# HRBuddy
+# core/logger.py
+
+# Required Libraries
+import logging
+import sys
+
+# Getting the logger
+def get_logger(name="HRBuddy"):
+    logger = logging.getLogger(name)
+    
+    # Checking if the logger has any handlers
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+        
+        # Formatting the log messages
+        formatter = logging.Formatter(
+            '[%(levelname)s] [%(asctime)s] [%(name)s]: %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        )
+        
+        # Adding the handler to the logger
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        
+    return logger
+
+log = get_logger()
